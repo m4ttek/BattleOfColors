9c66aef6d02cb42294b3ab2d36fe3e67a8f9613e
Mateusz <m4ttekaminski@gmail.com>
2014-10-31 01:02:20 +0100

    Początkowy szkielet engine'u gry.

---
+++ GameEngine/src/game_table/GameTable.java
@@ -0,0 +1,10 @@
+package game_table;
+
+import game_utils.Colors;
+
+import java.util.Collection;
+
+public interface GameTable {
+	
+	public Collection<Colors> getCurrentTable();
+}
---
+++ GameEngine/src/game_utils/PlayerType.java
@@ -0,0 +1,11 @@
+package game_utils;
+
+/**
+ * Typ wyliczeniowy dla typu gracza.
+ * 
+ * @author Mateusz Kamiński
+ */
+public enum PlayerType {
+	HUMAN,
+	AI
+}
