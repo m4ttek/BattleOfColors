f8273031ce17621b81e06fb982f5913b0de37b9b
Michał Pluta <123fokus@gmail.com>
2014-11-02 18:24:46 +0100

    Added some controller stuff and sample, very basic view presenting the idea

---
+++ BattleOfColorsWorkspace/BattleOfColors/src/game_table/DefaultGameTable.java
@@ -0,0 +1,15 @@
+package game_table;
+
+import game_utils.Colors;
+
+import java.util.Collection;
+
+public class DefaultGameTable implements GameTable {
+
+	@Override
+	public Collection<Colors> getCurrentTable() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
