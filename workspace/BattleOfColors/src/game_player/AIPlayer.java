23b644d35eb6734178978bb39a6d2fd487a22fec
Michał Pluta <123fokus@gmail.com>
2014-11-03 21:37:48 +0100

    Initial eclipse project commit

---
+++ workspace/BattleOfColors/src/game_player/AIPlayer.java
@@ -0,0 +1,53 @@
+package game_player;
+
+import game_table.GameTable;
+import game_utils.Colors;
+
+import java.awt.Color;
+import java.util.Collection;
+
+public class AIPlayer implements Player {
+
+	private final GameTable gameTable;
+
+	public AIPlayer(GameTable gameTable) {
+		this.gameTable = gameTable;
+	}
+	
+	@Override
+	public void makeMove() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setChosenColor(Color chosenColor) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Colors getPlayerColor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer getNumberOfFieldsTakenByPlayer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer getPlayerOriginPlace() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<Colors> getAvailableColorsForPlayer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
