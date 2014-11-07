a7fed326af3d650dfccebc8918bc21a030b8a3cf
Mateusz <m4ttekaminski@gmail.com>
2014-11-07 23:45:14 +0100

    Dodanie klasy IncorrectColorException.

---
+++ workspace/BattleOfColors/src/game_main/IncorrectColorException.java
@@ -0,0 +1,37 @@
+package game_main;
+
+import game_utils.Colors;
+
+/**
+ * 
+ * @author Mateusz Kamiński
+ */
+public class IncorrectColorException extends Exception {
+
+	/**
+	 * UID.
+	 */
+	private static final long serialVersionUID = -6842437809701040754L;
+	
+	private final Integer playerId;
+
+	private final Colors color;
+	
+	public IncorrectColorException(Integer playerId, Colors color) {
+		this.playerId = playerId;
+		this.color = color;
+	}
+
+	public Integer getPlayerId() {
+		return playerId;
+	}
+
+	public Colors getColor() {
+		return color;
+	}
+	
+	@Override
+	public String getMessage() {
+		return "Incorrect chosen color " + getColor() + " for player with id: " + getPlayerId();
+	}
+}
