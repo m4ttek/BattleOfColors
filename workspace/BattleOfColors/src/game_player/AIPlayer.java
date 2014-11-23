b73430b1e6091e87f4ab337b04a792b6d30bd6aa
123fokus <123fokus@gmail.com>
2014-11-23 19:55:05 +0100

    Update AIPlayer.java

--- workspace/BattleOfColors/src/game_player/AIPlayer.java
+++ workspace/BattleOfColors/src/game_player/AIPlayer.java
@@ -1,17 +1,23 @@
 package game_player;
 
+import game_main.GameState;
 import game_table.GameTable;
+import game_utils.Colors;
 
 import java.awt.Point;
 
-public class AIPlayer extends DefaultPlayer {
+public abstract class AIPlayer extends DefaultPlayer {
 
-	public AIPlayer(GameTable gameTable, Point playerStartingPosition) {
+	protected GameState gameState;
+	protected Colors opponentsColor;
+	protected Integer opponentsPlayerId;
+	
+	public AIPlayer(GameTable gameTable, Point playerStartingPosition, GameState gameState,
+			Integer opponentsPlayerId) {
 		super(gameTable, playerStartingPosition);
+		this.gameState = gameState;
+		this.opponentsPlayerId = opponentsPlayerId;
 	}
-
-	@Override
-	public void makeMove() {
-		
-	}
+	
+	public abstract void setDifficultyLevel(int level);
 }
